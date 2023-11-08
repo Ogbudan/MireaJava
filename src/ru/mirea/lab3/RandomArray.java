b77d9502fbb0a330bd0a3ee8088d3ec71b26c805
ogbudan <dvb011@yandex.ru>
2023-11-08 14:25:31 +0300

    лаб3 массив вещественных чисел и сортировка

--- src/ru/mirea/lab3/RandomArray.java
+++ src/ru/mirea/lab3/RandomArray.java
@@ -1,10 +1,12 @@
 package ru.mirea.lab3;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class RandomArray {
     public static void main(String[] args) {
         double[] numbers = new double[10];
+
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = Math.round(Math.random() * 100.0) / 100.0;
@@ -13,14 +15,17 @@ public class RandomArray {
         System.out.println("Массив до сортировки (метод random() класса Math): ");
         System.out.println(Arrays.toString(numbers));
 
-        Arrays.sort(numbers);
+        Arrays.sort(numbers); // Сортировка массива
 
         System.out.println("Массив после сортировки (метод random() класса Math): ");
         System.out.println(Arrays.toString(numbers));
 
 
+        Random random = new Random();
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = Math.round(Math.random() * 100.0) / 100.0;
+            numbers[i] = random.nextDouble() * 100;
+            numbers[i] = Math.round(numbers[i] * 100.0) / 100.0;
+
         }
 
         System.out.println("Массив до сортировки (класс Random): ");
@@ -29,6 +34,6 @@ public class RandomArray {
         Arrays.sort(numbers);
 
         System.out.println("Массив после сортировки (класс Random): ");
-
+        System.out.println(Arrays.toString(numbers));
     }
 }
