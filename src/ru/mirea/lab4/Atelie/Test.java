bf397c0ff0db9e24234ec7eb2f8636cacfa6760c
ogbudan <dvb011@yandex.ru>
2023-11-28 22:45:13 +0300

    Ателье

--- src/ru/mirea/lab4/Atelie/Test.java
+++ src/ru/mirea/lab4/Atelie/Test.java
@@ -3,18 +3,18 @@ package ru.mirea.lab4.Atelie;
 public class Test {
     public static void main(String[] args){
         Clothes[] clothes = {
-                new Tie(Size.S, 3000, Color.BLUE),
-                new TShirt(Size.M, 1500, Color.WHITE),
-                new Tie(Size.XS, 1000, Color.RED),
-                new TShirt(Size.L, 1500, Color.GREEN),
-                new Skirt(Size.S, 2000, Color.BLACK),
-                new Tie(Size.M, 5500, Color.WHITE),
-                new Skirt(Size.S, 8000, Color.PURPLE),
+                new Tie(Size.S, 3999, Color.BLUE),
+                new TShirt(Size.M, 1499, Color.WHITE),
+                new Tie(Size.XS, 999, Color.RED),
+                new TShirt(Size.L, 1699, Color.GREEN),
+                new Skirt(Size.S, 2999, Color.BLACK),
+                new Tie(Size.M, 4999, Color.WHITE),
+                new Skirt(Size.S, 9999, Color.PURPLE),
                 new TShirt(Size.XS, 2500, Color.ORANGE),
-                new Pants(Size.M, 4000, Color.YELLOW),
-                new Skirt(Size.L, 1500, Color.GRAY),
-                new Pants(Size.XXS, 4000, Color.BLUE),
-                new Tie(Size.XS, 7500, Color.WHITE)
+                new Pants(Size.M, 4999, Color.YELLOW),
+                new Skirt(Size.L, 1599, Color.GRAY),
+                new Pants(Size.XXS, 2999, Color.BLUE),
+                new Tie(Size.XS, 15999, Color.WHITE)
 
         };
 
